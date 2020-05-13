75f792f6fda19545d9d70070cbf86a307db5335f
TKMIT <takumiit110@gmail.com>
2020-05-13 14:17:17 +0900

    flaskのpythonファイル
    first commit

---
+++ ビジネス/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, logging, Response, redirect, flash
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run(host="localhost")
+ 
+# if __name__ == "__main__":
+#     app.run()
