6427506bd43a85d9b2223968ac75b3bdb577b9a0
TKMIT <takumiit110@gmail.com>
2020-05-13 16:57:37 +0900

    html操作、404追加
    2020/05/13

--- ビジネス/app.py
+++ ビジネス/app.py
@@ -5,7 +5,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-app.run(host="localhost")
+
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+
+if __name__ == "__main__":
+    app.run()
  
-# if __name__ == "__main__":
-#     app.run()
