052713843295cfebc1f9e5bf7371f9ed83e04f0d
aya_nakajou <aya.nakajou@veriserve.co.jp>
2020-05-20 11:57:01 +0900

    校正項目の制限が可能
    no_filterの実装

--- 0511 Lチーム.py
+++ 0511 Lチーム.py
@@ -1,9 +1,36 @@
-import requests
-url = 'https://jlp.yahooapis.jp/KouseiService/V1/kousei'
-response = requests.get(url)
+import urllib.parse
+import urllib.request
+import xml.etree.ElementTree as xml
+ 
+appid = "dj00aiZpPVhDRk8yQjZObTRZaiZzPWNvbnN1bWVyc2VjcmV0Jng9Y2M-"
+# 添削したい文章を入力
+a = "人事移動"
+sentence1 = urllib.parse.quote(a.encode('utf-8'))
+KOUSEI_NAMESPACE = '{urn:yahoo:jp:jlp:KouseiService}'
+FURIGANA_NAMESPACE = '{urn:yahoo:jp:jlp:FuriganaService}'
 
-print(response)
-# <Response [200]>
-
-print(type(response))
-# <class 'requests.models.Response'>
+# URLの生成（sentence以下は校正項目のフィルタリング）
+b = "https://jlp.yahooapis.jp/KouseiService/V1/kousei?appid="+appid+"&sentence="+sentence1+"&no_filter="
+ 
+recv = urllib.request.urlopen(b).read()
+data = xml.fromstring(recv)
+ 
+results = []
+for result in list(data):
+    startPos = int(result.findtext(KOUSEI_NAMESPACE + 'StartPos'))
+    length = int(result.findtext(KOUSEI_NAMESPACE + 'Length'))
+    endPos = startPos + length
+    surface = result.findtext(KOUSEI_NAMESPACE + 'Surface')
+    shitekiWord = result.findtext(KOUSEI_NAMESPACE + 'ShitekiWord')
+    shitekiInfo = result.findtext(KOUSEI_NAMESPACE + 'ShitekiInfo')
+ 
+    results.append({
+        'StartPos': startPos,
+        'EndPos': endPos,
+        'Length': length,
+        'Surface': surface,
+        'ShitekiWord': shitekiWord,
+        'ShitekiInfo': shitekiInfo,
+    })
+ 
+print(results)
