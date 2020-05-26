38eab92151a434573d8b8948ac880548fe5a2edd
hiroki-veriserve <hiroki.ida@veriserve.co.jp>
2020-05-26 13:38:03 +0900

    出力結果変更

--- doushi.py
+++ doushi.py
@@ -2,7 +2,6 @@ from janome.tokenizer import Tokenizer
 import csv
 import pandas as pd
 
-
 t = Tokenizer()
 tokens = t.tokenize(input("入力して下さい:"))
 for token in tokens:
@@ -13,14 +12,13 @@ for token in tokens:
         df= df.replace({'\n': '<br>'}, regex=True)
         df= df.replace({'\r': ''}, regex=True)
         df = df[df['見出し語'].str.contains(token.surface)]
-        # .emptyでCSVに入力されてない見出し語の場合に以下を出力
+        #.emptyでCSVに入力されてない見出し語の場合に以下を出力
         if df.empty:
             print('ご指定の語句には対応しておりません')
             break
-            # .drop 入力された名詞の削除
             # .to_stringインデックスの非表示
-        print(df.drop("見出し語",axis=1).to_string(index=False))
-
+        print(df.drop("見出し語",axis=1).to_string(index=False).replace("尊敬語","").replace("謙譲語","").replace("丁寧語",""))
+        # pprint.pprint(df.drop("見出し語",axis=1).to_string(index=False).replace("尊敬語","").replace("謙譲語","").replace("丁寧語",""))
     elif partOfSpeech =='名詞':
         ds = pd.read_csv('meishi.csv', encoding='utf_8', names=["見出し語","尊敬語","謙譲語","丁寧語"], usecols=[0,1,2,3], skiprows=[0], skipfooter=0, engine='python')
         ds= ds.replace({'\n': '<br>'}, regex=True)
@@ -29,8 +27,7 @@ for token in tokens:
         if ds.empty:
             print('ご指定の語句には対応しておりません')
             break
-        print(ds.drop("見出し語", axis=1).to_string(index=False))
-
+        print(ds.drop("見出し語", axis=1).to_string(index=False).replace("尊敬語","").replace("謙譲語","").replace("丁寧語",""))
     elif partOfSpeech =='助詞':
         ds = pd.read_csv('zyoshi.csv', encoding='utf_8', names=["見出し語","尊敬語","謙譲語","丁寧語"], usecols=[0,1,2,3], skiprows=[0], skipfooter=0, engine='python')
         ds= ds.replace({'\n': '<br>'}, regex=True)
@@ -39,8 +36,7 @@ for token in tokens:
         if ds.empty:
             print('ご指定の語句には対応しておりません')
             break
-        print(ds.drop("見出し語", axis=1).to_string(index=False))
-
+        print(ds.drop("見出し語", axis=1).to_string(index=False).replace("尊敬語","").replace("謙譲語","").replace("丁寧語",""))
     else :
         print("ご指定の語句には対応しておりません")
         break
