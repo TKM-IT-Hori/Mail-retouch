ce29d673adfad24041d7af0bfd18334e88578fd5
hiroki-veriserve <hiroki.ida@veriserve.co.jp>
2020-05-29 11:48:15 +0900

    出力結果変更

--- doushi.py
+++ doushi.py
@@ -13,13 +13,26 @@ def doushi(honorific):
             df= df.replace({'\n': '<br>'}, regex=True)
             df= df.replace({'\r': ''}, regex=True)
             df = df[df['見出し語'].str.contains(token.surface)]
+     
             #.emptyでCSVに入力されてない見出し語の場合に以下を出力
             if df.empty:
                 response_empty='<font color="red">ご指定の語句には対応しておりません</font>'
                 return response_empty
-                # .to_stringインデックスの非表示
-            response_string=df.drop("見出し語",axis=1).to_string(index=False).replace("尊敬語","").replace("謙譲語","").replace("丁寧語","")
-            return response_string
+
+            #尊敬語配列
+            son=df["尊敬語"].to_string(index=False).replace("\n","").replace("NaN","").replace("'","")
+            s=[son]
+            #謙譲語配列
+            ken=df["謙譲語"].to_string(index=False).replace("\n","").replace("NaN","")
+            k=[ken]
+            #丁寧語配列
+            tei=df["丁寧語"].to_string(index=False).replace("\n","").replace("NaN","")
+            t=[tei]
+            return s,k,t
+
+            # response_string=df.drop("見出し語",axis=1).to_string(index=False)
+            # response_string={df.drop("見出し語",axis=1).to_string(index=False).replace("尊敬語","").replace("謙譲語","").replace("丁寧語","")}
+            # return response_string
             #pprint.pprint(df.drop("見出し語",axis=1).to_string(index=False).replace("尊敬語","").replace("謙譲語","").replace("丁寧語",""))
         
         elif partOfSpeech =='名詞':
@@ -30,9 +43,19 @@ def doushi(honorific):
             if ds.empty:
                 response_empty='<font color="red">ご指定の語句には対応しておりません</font>'
                 return response_empty
-            response_string=ds.drop("見出し語", axis=1).to_string(index=False).replace("尊敬語","").replace("謙譲語","").replace("丁寧語","")
-            return response_string
-
+            #尊敬語配列
+            son=ds["尊敬語"].to_string(index=False).replace("\n","").replace("NaN","").replace("'","")
+            s=[son]
+            #謙譲語配列
+            ken=ds["謙譲語"].to_string(index=False).replace("\n","").replace("NaN","")
+            k=[ken]
+            #丁寧語配列
+            tei=ds["丁寧語"].to_string(index=False).replace("\n","").replace("NaN","")
+            t=[tei]
+            return s,k,t
+            # response_string=ds.drop("見出し語", axis=1).to_string(index=False).replace("尊敬語","").replace("謙譲語","").replace("丁寧語","")
+            # return response_string
+        
         elif partOfSpeech =='助詞':
             ds = pd.read_csv('zyoshi.csv', encoding='utf_8', names=["見出し語","尊敬語","謙譲語","丁寧語"], usecols=[0,1,2,3], skiprows=[0], skipfooter=0, engine='python')
             ds= ds.replace({'\n': '<br>'}, regex=True)
@@ -41,8 +64,19 @@ def doushi(honorific):
             if ds.empty:
                 response_empty='<font color="red">ご指定の語句には対応しておりません</font>'
                 return response_empty
-            response_string=ds.drop("見出し語", axis=1).to_string(index=False).replace("尊敬語","").replace("謙譲語","").replace("丁寧語","")
-            return response_string
+
+            #尊敬語配列
+            son=ds["尊敬語"].to_string(index=False).replace("\n","").replace("NaN","").replace("'","")
+            s=[son]
+            #謙譲語配列
+            ken=ds["謙譲語"].to_string(index=False).replace("\n","").replace("NaN","")
+            k=[ken]
+            #丁寧語配列
+            tei=ds["丁寧語"].to_string(index=False).replace("\n","").replace("NaN","")
+            t=[tei]
+            return s,k,t
+            # response_string=ds.drop("見出し語", axis=1).to_string(index=False).replace("尊敬語","").replace("謙譲語","").replace("丁寧語","")
+            # return response_string
         else :
             response_error='<font color="red">ご指定の語句には対応しておりません</font>'
             return response_error
